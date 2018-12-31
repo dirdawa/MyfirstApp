5f5a52d87fcee6d83dd79f7532448f275fba698b
Solomon Reta <dirdawa@gmail.com>
2018-12-31 11:40:16 -0600

    Add files via upload

---
+++ CellPhone.cs
@@ -0,0 +1,63 @@
+namespace classwork
+{
+    public class CellPhone
+
+    {
+        // The following are fields
+        string _voice;
+        string _data;
+        string _text;
+
+        public string Voice
+        {
+            get
+            {
+                return _answercall;
+            }
+            set
+            {
+                this._answercall = value;
+            }
+        }
+
+        public void ClassRoom()
+        {
+
+        }
+
+
+        public void EndCall()
+        
+
+
+        // The following are Method
+
+        public string Data
+        {
+            get
+            {
+                return _dataon;
+            }
+            set
+            {
+                this._dataoff = value;
+            }
+        }
+
+        public void OpenDoor()
+        {
+
+        }
+
+        public void ClosedDoor()
+        {
+
+        }
+
+
+
+        // The following are Method
+        public string Text { get; set; }
+
+    }
+}
